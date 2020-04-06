d48b36a5037b88590f12ba9a1d82039ec48dcb57
ana <anameciarobalo@gmail.com>
2020-04-06 20:13:38 +0100

    add style to the home component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Skills from './components/Skills'
 import Contacts from './components/Contacts'
-import AppCss from './App.css'
+import './App.css'
 
 
 function App() {
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../styles/Home.css'
 
 const Home = () => {
     return(
-        <div>
+        <div className='home-container'>
             <h1>Hi, I am Ana</h1>
             <h1>Full Stack Developer</h1>
         </div>
