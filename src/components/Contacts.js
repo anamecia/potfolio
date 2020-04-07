1a910a53487b1116bb159289f3607fd27db8be24
ana <anameciarobalo@gmail.com>
2020-04-07 22:22:42 +0100

    add icons to contact section

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import linkedinIcon from '../assets/images/linkedin.svg'
+import mediumIcon from '../assets/images/medium.svg'
+import githubIcon from '../assets/images/github.svg'
+
+import '../styles/Contacts.css'
 
 const Contacts = () => {
     return(
-        <div>
-            <h3>Get in touch ...</h3>
-            <a href='https://www.linkedin.com/in/anameciarobalo/'>linkedin</a>
-            <a href='https://github.com/anamecia>github'>github</a>
-            <a href='https://medium.com/@anameciarobalo'>medium</a>
-
-
+        <div className='contacts-main-container'>
+            <div className='contacts-subcontainer'>
+                <h3>Get in touch ...</h3>
+                <div className='links-container'>
+                    <a href='https://www.linkedin.com/in/anameciarobalo/'><img className='link' src={linkedinIcon} alt='linkedin icon'/></a>
+                    <a href='https://github.com/anamecia>github'><img className='link' src={githubIcon} alt='github icon'/></a>
+                    <a href='https://medium.com/@anameciarobalo'><img className='link' src={mediumIcon} alt='medium icon'/></a>
+                </div>
+            </div>
         </div>
     )
 }
