8b1c0dd2404f9eaaf71f9cf43582ea9336a99eaf
ana <anameciarobalo@gmail.com>
2020-04-08 11:40:44 +0100

    add greeting to the contact section

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,6 +2,7 @@ import React from 'react'
 import linkedinIcon from '../assets/images/linkedin.svg'
 import mediumIcon from '../assets/images/medium.svg'
 import githubIcon from '../assets/images/github.svg'
+import wavingHand from '../assets/images/waving-hand.svg'
 
 import '../styles/Contacts.css'
 
@@ -14,6 +15,10 @@ const Contacts = () => {
                     <a href='https://www.linkedin.com/in/anameciarobalo/'><img className='link' src={linkedinIcon} alt='linkedin icon'/></a>
                     <a href='https://github.com/anamecia>github'><img className='link' src={githubIcon} alt='github icon'/></a>
                     <a href='https://medium.com/@anameciarobalo'><img className='link' src={mediumIcon} alt='medium icon'/></a>
+                </div>
+                <div className='greeting-container'>
+                    <img className='greeting'src={wavingHand} alt='waving hand'/>
+                    <p>Nice to meet you!</p>
                 </div>
             </div>
         </div>
