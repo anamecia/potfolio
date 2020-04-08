f2d29473b4eb69c90c018659d5fae9d22947a6fb
ana <anameciarobalo@gmail.com>
2020-04-08 17:11:51 +0100

    add style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,56 @@
 import React from 'react'
 import { Link } from 'react-scroll'
+import '../styles/Navbar.css'
 
 
 const NavBar = () => {
     return(
         <div className='navbar-container'>
-            <Link
-                activeClass="active"
-                to="section1"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration= {500}
-            > Home </Link>
-            <Link
-                activeClass="active"
-                to="section1"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration= {500}
-            > About </Link>
-            <Link
-                activeClass="active"
-                to="section1"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration= {500}
-            > Skills</Link>
-            <Link
-                activeClass="active"
-                to="section1"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration= {500}
-            > Projects </Link>
-            <Link
-                activeClass="active"
-                to="section1"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration= {500}
-            > Contacts </Link>
-
+            <div className='logo-container'>
+                <h1>Logo</h1>
+            </div>
+            <div className='link-container'>
+                <Link
+                    activeClass="active"
+                    to="section1"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration= {500}
+                > Home </Link>
+                <Link
+                    activeClass="active"
+                    to="section1"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration= {500}
+                > About </Link>
+                <Link
+                    activeClass="active"
+                    to="section1"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration= {500}
+                > Skills</Link>
+                <Link
+                    activeClass="active"
+                    to="section1"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration= {500}
+                > Projects </Link>
+                <Link
+                    activeClass="active"
+                    to="section1"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration= {500}
+                > Contacts </Link>
+            </div>
         </div>
     )
 }
