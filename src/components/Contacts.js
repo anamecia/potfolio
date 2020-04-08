925b3281f8778ccbe1d9dee8d5e2b8b5f26724f8
ana <anameciarobalo@gmail.com>
2020-04-08 11:14:31 +0100

    change the text above links in the contact section

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -9,7 +9,7 @@ const Contacts = () => {
     return(
         <div className='contacts-main-container'>
             <div className='contacts-subcontainer'>
-                <h3>Get in touch ...</h3>
+                <h3>Do you want to work in a project together or just have a chat ...</h3>
                 <div className='links-container'>
                     <a href='https://www.linkedin.com/in/anameciarobalo/'><img className='link' src={linkedinIcon} alt='linkedin icon'/></a>
                     <a href='https://github.com/anamecia>github'><img className='link' src={githubIcon} alt='github icon'/></a>
