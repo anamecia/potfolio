61a44787d8b9d7bc08f3a61332db8116dff181eb
ana <anameciarobalo@gmail.com>
2020-04-21 17:30:24 +0100

    change style in the contacts section

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -9,18 +9,19 @@ import '../styles/Contacts.css'
 const Contacts = () => {
     return(
         <div className='contacts-main-container' id='contacts'>
-            <div className='contacts-subcontainer'>
-                <h3>Do you want to work in a project together or just have a chat ...</h3>
+            <div className='contact-links-container'>
+                <p>Do you want to work in a project together or just have a chat ...</p>
                 <div className='links-container'>
                     <a href='https://www.linkedin.com/in/anameciarobalo/'><img className='link' src={linkedinIcon} alt='linkedin icon'/></a>
                     <a href='https://github.com/anamecia>github'><img className='link' src={githubIcon} alt='github icon'/></a>
                     <a href='https://medium.com/@anameciarobalo'><img className='link' src={mediumIcon} alt='medium icon'/></a>
                 </div>
-                <div className='greeting-container'>
-                    <img className='greeting'src={wavingHand} alt='waving hand'/>
-                    <p>Nice to meet you!</p>
-                </div>
             </div>
+            <div className='greeting-container'>
+                <img className='greeting'src={wavingHand} alt='waving hand'/>
+                <p>Nice to meet you!</p>
+            </div>
+         
         </div>
     )
 }
